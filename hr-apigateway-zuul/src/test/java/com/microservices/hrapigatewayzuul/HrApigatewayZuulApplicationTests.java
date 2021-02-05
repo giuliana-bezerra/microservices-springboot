5eb871a7f6b0f1c3f3048c96c232ed7bfd891d53
Giuliana Silva Bezerra <giu.drawer@gmail.com>
2021-02-05 15:01:29 -0300

    add hr-apigateway-zuul

---
+++ hr-apigateway-zuul/src/test/java/com/microservices/hrapigatewayzuul/HrApigatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApigatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
