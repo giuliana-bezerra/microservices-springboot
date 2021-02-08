44b1523c815456c8dd8086ebf902e4e9b38227f8
Giuliana Silva Bezerra <giu.drawer@gmail.com>
2021-02-08 14:54:14 -0300

    add database scripts

--- hr-user/src/main/java/com/microservices/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/microservices/hruser/HrUserApplication.java
@@ -1,15 +1,22 @@
 package com.microservices.hruser;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class HrUserApplication {
+public class HrUserApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HrUserApplication.class, args);
+	}
+
+	// Generating bcrypt password!
+	@Override
+	public void run(String... args) throws Exception {
+		// System.out.println("BCRYPT = " + new BCryptPasswordEncoder().encode("123456"));
 	}
 
 }
