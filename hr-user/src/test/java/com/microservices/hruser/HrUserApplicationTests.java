218cd41114a44cafeb01ee122a4bd45479b0a16e
Giuliana Silva Bezerra <giu.drawer@gmail.com>
2021-02-08 11:42:36 -0300

    add hr-user

---
+++ hr-user/src/test/java/com/microservices/hruser/HrUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.hruser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
