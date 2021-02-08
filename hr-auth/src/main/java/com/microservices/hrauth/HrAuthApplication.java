ba17a4c58b53344300da94b3d8cd528c2dff011f
Giuliana Silva Bezerra <giu.drawer@gmail.com>
2021-02-08 15:58:59 -0300

    add hr-auth

---
+++ hr-auth/src/main/java/com/microservices/hrauth/HrAuthApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.hrauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrAuthApplication.class, args);
+	}
+
+}
---
+++ hr-auth/src/test/java/com/microservices/hrauth/HrAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.hrauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
