b581e68dbce7e50306960edf82d43d8d6f615608
Giuliana Silva Bezerra <giu.drawer@gmail.com>
2021-02-08 10:12:23 -0300

    config actuator for changing config at runtime

--- hr-worker/src/main/java/com/microservices/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/microservices/hrworker/resources/WorkerResource.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.microservices.hrworker.entities.Worker;
 import com.microservices.hrworker.repositories.WorkerRepository;
 
+@RefreshScope
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerResource {
